e065530ae16e6f85a70ace300608a7cb13388ff3
Darron Froese <darron@froese.org>
2013-01-09 17:26:44 -0700

    Disabled mysql password generation.

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,15 +1,15 @@
 package "makepasswd"
 
-bash "setup mysql password" do
-  user "root"
-  cwd "/root"
-  code <<-EOH
-    userPassword=$(makepasswd --char=10) # Generate a random MySQL password
-    echo mysql-server mysql-server/root_password password $userPassword | sudo debconf-set-selections
-    echo mysql-server mysql-server/root_password_again password $userPassword | sudo debconf-set-selections
-  EOH
-  not_if { FileTest.exists?("/usr/sbin/mysqld") }
-end
+# bash "setup mysql password" do
+#   user "root"
+#   cwd "/root"
+#   code <<-EOH
+#     userPassword=$(makepasswd --char=10) # Generate a random MySQL password
+#     echo mysql-server mysql-server/root_password password $userPassword | sudo debconf-set-selections
+#     echo mysql-server mysql-server/root_password_again password $userPassword | sudo debconf-set-selections
+#   EOH
+#   not_if { FileTest.exists?("/usr/sbin/mysqld") }
+# end
 
 package "mysql-server"
 package "mysql-client"
